c2a93a841598f7f1ec0831431fec8a033562f442
brje <juho-joel.luukkonen@student.lut.fi>
2021-04-23 19:24:33 +0300

    muutoksia homeframentissä

--- app/src/main/java/com/example/projectx/HomeFragment.java
+++ app/src/main/java/com/example/projectx/HomeFragment.java
@@ -26,10 +26,10 @@ public class HomeFragment extends Fragment {
         paramView = inflater.inflate(R.layout.fragment_home, container, false);
         mpLineChart = (LineChart) paramView.findViewById(R.id.chart);
 
-        LineDataSet lineDataSet1 = new LineDataSet(dataValues1(), "Data set 1");
+        LineDataSet lineDataSet1 = new LineDataSet(userBmiValues(), "Oma BMI");
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
         dataSets.add(lineDataSet1);
-        LineDataSet lineDataSet2 = new LineDataSet(dataValues2(), "Data set 2");
+        LineDataSet lineDataSet2 = new LineDataSet(whoBmiConstant(), "Vertailu BMI");
         dataSets.add(lineDataSet2);
         LineData data = new LineData(dataSets);
         mpLineChart.setData(data);
@@ -38,29 +38,30 @@ public class HomeFragment extends Fragment {
         return paramView;
     }
 
-    private ArrayList<Entry> dataValues1()
+    private ArrayList<Entry> userBmiValues()
     {
         //TODO: Lisää profiililta paino ja päivänmäärä data tähän. (x=bmi, y= pvm)
-        ArrayList<Entry> dataVals = new ArrayList<Entry>();
-        dataVals.add(new Entry(0, 20));
-        dataVals.add(new Entry(1, 24));
-        dataVals.add(new Entry(2, 5));
-        dataVals.add(new Entry(3, 10));
-        dataVals.add(new Entry(4, 25));
+        ArrayList<Entry> userBmiVals = new ArrayList<Entry>();
+        userBmiVals.add(new Entry(0, 20));
+        userBmiVals.add(new Entry(1, 24));
+        userBmiVals.add(new Entry(2, 5));
+        userBmiVals.add(new Entry(3, 10));
+        userBmiVals.add(new Entry(4, 25));
 
-        return dataVals;
+        return userBmiVals;
     }
 
-    private ArrayList<Entry> dataValues2()
+    private ArrayList<Entry> whoBmiConstant()
     {
         //TODO: Lisää profiililta paino ja päivänmäärä data tähän.
-        ArrayList<Entry> dataVals = new ArrayList<Entry>();
-        dataVals.add(new Entry(0, 25));
-        dataVals.add(new Entry(1, 25));
-        dataVals.add(new Entry(2, 25));
-        dataVals.add(new Entry(3, 25));
-        dataVals.add(new Entry(4, 25));
+        ArrayList<Entry> whoBmiVals = new ArrayList<Entry>();
+        //TODO: for-looppi, jolla lisätään pvmt x ja whoAPI tieto vakiona y
+        whoBmiVals.add(new Entry(0, 25));
+        whoBmiVals.add(new Entry(1, 25));
+        whoBmiVals.add(new Entry(2, 25));
+        whoBmiVals.add(new Entry(3, 25));
+        whoBmiVals.add(new Entry(4, 25));
 
-        return dataVals;
+        return whoBmiVals;
     }
 }
