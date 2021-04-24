14d74a0eef19f0348fb4e108670d37ea220b0ae7
Paddy-622 <77283580+Paddy-622@users.noreply.github.com>
2021-04-24 17:19:20 +0300

    Revert "Muutoksia kuvaajaan"
    This reverts commit cd24d818

--- app/src/main/java/com/example/projectx/RegisterFragment.java
+++ app/src/main/java/com/example/projectx/RegisterFragment.java
@@ -8,17 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class RegisterFragment extends Fragment implements View.OnClickListener{
 
     Button signUpButton;
+    Spinner spinner;
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_register, container, false);
         signUpButton = (Button) v.findViewById(R.id.btn_sign);
+        Spinner spinner = v.findViewById(R.id.et_sexSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.genders_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         return v;
     }
 
