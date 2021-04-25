8c73814cd5c4bc071fffd2c4fb498036d3c44510
Paddy-622 <77283580+Paddy-622@users.noreply.github.com>
2021-04-26 01:06:09 +0300

    Info fragment layout luotu

--- app/src/main/java/com/example/projectx/fragments/InfoFragment.java
+++ app/src/main/java/com/example/projectx/fragments/InfoFragment.java
@@ -19,7 +19,7 @@ public class InfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_info, container, false);
 
 
         return v;
