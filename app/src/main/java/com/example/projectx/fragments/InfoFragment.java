f1e1b72553aa669d81d27f3aa435fbb0d14f686b
Timpp4 <tuomas.ikone@gmail.com>
2021-04-26 21:37:12 +0300

    InfoFragment.java toiminnallisuus luotu

--- app/src/main/java/com/example/projectx/fragments/InfoFragment.java
+++ app/src/main/java/com/example/projectx/fragments/InfoFragment.java
@@ -9,19 +9,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.projectx.R;
+import com.example.projectx.backend.readAndWrite;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class InfoFragment extends Fragment {
+public class InfoFragment extends Fragment implements View.OnClickListener {
+
+    private Button submit;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_info, container, false);
-
+        submit = (Button) v.findViewById((R.id.info_submit));
+        submit.setOnClickListener(this);
+        displayData(v);
 
         return v;
     }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == submit.getId()) {
+            readAndWrite rw = new readAndWrite(getContext());
+            TextView tvId = getView().findViewById(R.id.info_id_remove);
+            TextView tvData = getView().findViewById(R.id.info_data);
+            System.out.println(tvId.getText().toString());
+            int id = Integer.parseInt(tvId.getText().toString());
+            tvData.setText(rw.removeData(id));
+
+
+
+
+
+
+        }
+    }
+
+    // Display user data
+    public void displayData(View view) {
+        readAndWrite rw = new readAndWrite(getContext());
+        String contents = rw.displayData();
+        TextView tv = view.findViewById(R.id.info_data);
+        tv.setText(contents);
+    }
+
+
+
+
+
 }
