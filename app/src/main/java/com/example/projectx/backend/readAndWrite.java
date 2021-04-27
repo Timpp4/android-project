a99a3bfda8918761123f53637cdabbb6e797ada6
Timpp4 <tuomas.ikone@gmail.com>
2021-04-27 19:13:49 +0300

    readAndWrite.java kommentointia lisätty ja varoituksia korjattu

--- app/src/main/java/com/example/projectx/backend/readAndWrite.java
+++ app/src/main/java/com/example/projectx/backend/readAndWrite.java
@@ -2,30 +2,28 @@ package com.example.projectx.backend;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 public class readAndWrite {
 
-
-    private Context context;
+    private final Context context;
     public readAndWrite(Context context) { this.context=context; }
-    //public readAndWrite(RegisterFragment context) { this.context=context; }
 
-    // Parse through userInfo.txt to validate given username and password for app access
+    /**
+     * This function is used to log user in and
+     * creates a temporary file to identify user.
+     */
     public boolean readFile(String username, String password) {
         try {
             File path = context.getExternalFilesDir(null);
-            System.out.println("path is ... " + path);
             File file = new File(path, "userInfo.txt");
             int length = (int) file.length();
             byte[] bytes = new byte[length];
@@ -33,13 +31,11 @@ public class readAndWrite {
             in.read(bytes);
             in.close();
             String contents = new String(bytes);
-
             String[] line_parsed;
             line_parsed = contents.split("\n"); // Row
             int arrayLength;
             int i = 0;
             arrayLength = line_parsed.length;
-
             while (i < arrayLength) {
                 String user = line_parsed[i];
                 String[] user_parsed;
@@ -52,25 +48,26 @@ public class readAndWrite {
                             stream.write(username.getBytes());
                             stream.close();
                         } catch (Exception e) {
-                            System.out.println(e);
+                            e.printStackTrace();
                         }
-                        System.out.println("You have successfully logged in! (readandwrite.java)");
                         return true;
                     }
                 }
                 i++;
             }
-
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
         return false;
     }
 
+    /**
+     * This function checks if username is taken.
+     * It is called when a new user is created.
+     */
     public boolean checkIfUserExist(String username) {
         try {
             File path = context.getExternalFilesDir(null);
-
             File file = new File(path, "userInfo.txt");
             int length = (int) file.length();
             byte[] bytes = new byte[length];
@@ -78,71 +75,65 @@ public class readAndWrite {
             in.read(bytes);
             in.close();
             String contents = new String(bytes);
-
             String[] line_parsed;
             line_parsed = contents.split("\n"); // rivi
             int arrayLength;
             int i = 0;
             arrayLength = line_parsed.length;
-
             while (i < arrayLength) {
                 String user = line_parsed[i];
                 String[] user_parsed;
                 user_parsed = user.split(";");
-                //System.out.println(user_parsed[0] + " ja " + user_parsed[1]);
                 if (user_parsed[0].equals(username)) {
-                    System.out.println("kyseinen käyttäjä löytyi....");
                     return true;
                 }
                 i++;
             }
-
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-
         return false;
     }
 
-    // Save newly created username and password for login
+    /**
+     * This function saves newly created username
+     * and password for login.
+     */
     public boolean writeFile(String username, String password) {
         if (username.trim().length() == 0 || password.length() < 12){
             return false;
         }
-        if (checkIfUserExist(username) == true) {
-            System.out.println("checkifuserexist returned true, dont continue");
+        if (checkIfUserExist(username)) {
             return false;
         }
         String row = username + ";" + password + "\n";
         try {
             File path = context.getExternalFilesDir(null);
-            System.out.println("path is ... " + path);
             File file = new File(path, "userInfo.txt");
             FileOutputStream stream = new FileOutputStream(file, true);
             stream.write(row.getBytes());
             stream.close();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-        System.out.println("writefilussa");
         return true;
     }
 
+    /**
+     * This function creates a new user. It takes parameters from
+     * the user and check if they are valid.
+     */
     public boolean createNewUser(String username, String password, Double height, Double weight, Integer yearBorn, String sex) {
         if (height < 0 || weight < 0 || yearBorn < 0){
-            System.out.println("Tiedot epätäydellisiä");
             return false;
         }
-        // Save username and password to a separate file for login
-        if (writeFile(username, password) == false) {
-            System.out.println("ei voitu kirjoitella kyättistä ja salasanaa, exit and forget");
+        if (!writeFile(username, password)) {
             return false;
         }
         try {
             String row = username + ";" + height.toString() + ";" + weight.toString() + ";" + yearBorn.toString() + ";" + sex.toLowerCase() + "\n";
-            String date = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+            @SuppressLint("SimpleDateFormat") String date = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
             String row_dateWeight = date + ";" + weight.toString() + "\n";
-
             File path = context.getExternalFilesDir(null);
             File file = new File(path, username + ".txt");
             FileOutputStream stream = new FileOutputStream(file, true);
@@ -150,15 +141,18 @@ public class readAndWrite {
             stream.write(row_dateWeight.getBytes());
             stream.close();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-        System.out.println("käyttäjä luotiin onnistuneesti");
         return true;
     }
+
+    /**
+     * This function reads user's personal data for BMI calculation
+     * and graphical data.
+     */
     public ArrayList<DataObject> readUserData() {
-        //TODO: Tee usernamen etsintä
         String username = getUsername();
-        ArrayList<DataObject> dataObject = new ArrayList<DataObject>();
+        ArrayList<DataObject> dataObject = new ArrayList<>();
         try {
             File path = context.getExternalFilesDir(null);
             File file = new File(path, username + ".txt");
@@ -168,13 +162,11 @@ public class readAndWrite {
             in.read(bytes);
             in.close();
             String contents = new String(bytes);
-
             String[] line_parsed;
             line_parsed = contents.split("\n"); // rivi
             int arrayLength;
             int i = 0;
             arrayLength = line_parsed.length;
-
             while (i < arrayLength) {
                 if (i != 0){
                     String user = line_parsed[i];
@@ -188,49 +180,38 @@ public class readAndWrite {
                 i++;
             }
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
         Collections.sort(dataObject);
-
         return dataObject;
     }
 
+    /**
+     * This function reads user's input from home view and
+     * saves it to a file.
+     */
     public void insertWeight(String date, Double weight) {
         try {
-            //Find where to write
-            File path = context.getExternalFilesDir(null);
-            File file = new File(path, "tmp.txt");
-            int length = (int) file.length();
-            byte[] bytes = new byte[length];
-            FileInputStream in = new FileInputStream(file);
-            in.read(bytes);
-            in.close();
-            String contents = new String(bytes); //Username = contents
-
+            String contents = getUsername();
             String row = date + ";" + weight + "\n";
-
+            File path = context.getExternalFilesDir(null);
             File file_insertWeight = new File(path, contents + ".txt");
             FileOutputStream stream = new FileOutputStream(file_insertWeight, true);
             stream.write(row.getBytes());
             stream.close();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-
     }
 
+    /**
+     * This function reads user's personal data for profile view.
+     */
     public List<String> profileInfo() {
-        List<String> infoList = new ArrayList<String>();
+        List<String> infoList = new ArrayList<>();
         try {
+            String username = getUsername();
             File path = context.getExternalFilesDir(null);
-            File file_tmp = new File(path, "tmp.txt");
-            int length_tmp = (int) file_tmp.length();
-            byte[] bytes_tmp = new byte[length_tmp];
-            FileInputStream in_tmp = new FileInputStream(file_tmp);
-            in_tmp.read(bytes_tmp);
-            in_tmp.close();
-            String username = new String(bytes_tmp);
-
             File file = new File(path, username + ".txt");
             int length = (int) file.length();
             byte[] bytes = new byte[length];
@@ -239,12 +220,10 @@ public class readAndWrite {
             in.close();
             String contents = new String(bytes);
             String[] line_parsed;
-            line_parsed = contents.split("\n"); // rivi
+            line_parsed = contents.split("\n"); // row
             int arrayLength;
             int i = 0;
             arrayLength = line_parsed.length;
-
-            System.out.println(arrayLength);
             while (i < arrayLength) {
                 String row = line_parsed[i];
                 String[] row_parsed;
@@ -261,20 +240,18 @@ public class readAndWrite {
                 i++;
             }
         } catch (Exception e) {
-            System.out.println("profiili error " + e);
+            e.printStackTrace();
         }
         return infoList;
     }
 
     /**
      * This function finds user in-use at the time from tmp.txt file
-     * @file uses tmp.txt file which include user in-use
+     * file uses tmp.txt file which include user in-use
      * @return username in string
      */
-    public String getUsername (){
-
+    public String getUsername () {
         String username="";
-
         try {
             File path = context.getExternalFilesDir(null);
             File file = new File(path, "tmp.txt");
@@ -283,13 +260,17 @@ public class readAndWrite {
             FileInputStream in = new FileInputStream(file);
             in.read(bytes);
             in.close();
-            username = new String(bytes); //Username = contents
+            username = new String(bytes);
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
         return username;
     }
 
+    /**
+     * This function reads user's personal data
+     * and displays it in the info view.
+     */
     public String displayData() {
         String username = getUsername();
         String profile_data="";
@@ -302,24 +283,26 @@ public class readAndWrite {
             in.read(bytes);
             in.close();
             String contents = new String(bytes);
-
             String[] line_parsed;
             line_parsed = contents.split("\n"); // Row
             int arrayLength;
             int i = 0;
             arrayLength = line_parsed.length;
-
             while (i < arrayLength) {
                 profile_data = profile_data + i + " --- " + line_parsed[i] + "\n";
                 i++;
             }
-
         } catch (Exception e) {
-            System.out.println("vIRHE DISPLAYDATA() " + e);
+            e.printStackTrace();
         }
         return profile_data;
     }
 
+    /**
+     * This function removes personal data with corresponding id.
+     * It then displays the new data in the info view and
+     * updates user's personal data file.
+     */
     public String removeData(Integer id) {
         String username = getUsername();
         String profile_data="";
@@ -333,14 +316,12 @@ public class readAndWrite {
             in.read(bytes);
             in.close();
             String contents = new String(bytes);
-
             String[] line_parsed;
             line_parsed = contents.split("\n"); // Row
             int arrayLength;
             int i = 0;
             arrayLength = line_parsed.length;
             boolean idBoolean = false;
-
             while (i < arrayLength) {
                 if (i==0) {
                     profile_data = profile_data + i + " --- " + line_parsed[i] + "\n";
@@ -349,7 +330,7 @@ public class readAndWrite {
                     idBoolean = true;
                 } else if (id != i) {
                     profile_data_toFile = profile_data_toFile + line_parsed[i] + "\n";
-                    if (idBoolean == false) {
+                    if (!idBoolean) {
                         profile_data = profile_data + i + " --- " + line_parsed[i] + "\n";
                     } else {
                         profile_data = profile_data + (i-1) + " --- " + line_parsed[i] + "\n";
@@ -357,23 +338,13 @@ public class readAndWrite {
                 }
                 i++;
             }
-
             File fileNew = new File(path, username + ".txt");
             FileOutputStream stream = new FileOutputStream(file);
             stream.write(profile_data_toFile.getBytes());
             stream.close();
-
         } catch (Exception e) {
-            System.out.println("vIRHE removedata() " + e);
+            e.printStackTrace();
         }
         return profile_data;
     }
-
-
-
-
-
-
-
-
 }
